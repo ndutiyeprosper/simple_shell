b4850da86318e417742d1e404c85d0caf265cff6
ndutiyeprosper <123053246+ndutiyeprosper@users.noreply.github.com>
2023-04-27 02:52:22 +0200

    Update exits.c

--- exits.c
+++ exits.c
@@ -1,12 +1,5 @@
 #include "shell.h"
 
-/**
- **_strncpy - copies a string
- *@dest: the destination string to be copied to
- *@src: the source string
- *@n: the amount of characters to be copied
- *Return: the concatenated string
- */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i, j;
@@ -30,13 +23,6 @@ char *_strncpy(char *dest, char *src, int n)
 	return (s);
 }
 
-/**
- **_strncat - concatenates two strings
- *@dest: the first string
- *@src: the second string
- *@n: the amount of bytes to be maximally used
- *Return: the concatenated string
- */
 char *_strncat(char *dest, char *src, int n)
 {
 	int i, j;
@@ -57,12 +43,7 @@ char *_strncat(char *dest, char *src, int n)
 	return (s);
 }
 
-/**
- **_strchr - locates a character in a string
- *@s: the string to be parsed
- *@c: the character to look for
- *Return: (s) a pointer to the memory area s
- */
+
 char *_strchr(char *s, char c)
 {
 	do {
