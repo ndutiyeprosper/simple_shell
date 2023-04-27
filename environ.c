aac9d67aea2157f7e0bcce944f0ad7a839e1e4ad
ndutiyeprosper <pr.ndutiye@gmail.com>
2023-04-26 17:22:55 -0700

    env

--- environ.c
+++ environ.c
@@ -1,24 +1,11 @@
 #include "shell.h"
 
-/**
- * _myenv - prints the current environment
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
 int _myenv(info_t *info)
 {
 	print_list_str(info->env);
 	return (0);
 }
 
-/**
- * _getenv - gets the value of an environ variable
- * @info: Structure containing potential arguments. Used to maintain
- * @name: env var name
- *
- * Return: the value
- */
 char *_getenv(info_t *info, const char *name)
 {
 	list_t *node = info->env;
@@ -34,18 +21,11 @@ char *_getenv(info_t *info, const char *name)
 	return (NULL);
 }
 
-/**
- * _mysetenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
 int _mysetenv(info_t *info)
 {
 	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguements\n");
+		_eputs("Probably  there are someIncorrect number of arguements\n");
 		return (1);
 	}
 	if (_setenv(info, info->argv[1], info->argv[2]))
@@ -53,19 +33,13 @@ int _mysetenv(info_t *info)
 	return (1);
 }
 
-/**
- * _myunsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: Always 0
- */
 int _myunsetenv(info_t *info)
 {
 	int i;
 
 	if (info->argc == 1)
 	{
-		_eputs("Too few arguements.\n");
+		_eputs("Too few arguements, Pleae add more.\n");
 		return (1);
 	}
 	for (i = 1; i <= info->argc; i++)
@@ -74,12 +48,6 @@ int _myunsetenv(info_t *info)
 	return (0);
 }
 
-/**
- * populate_env_list - populates env linked list
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- * Return: Always 0
- */
 int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
