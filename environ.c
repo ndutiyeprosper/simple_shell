38bb1bf458e02863c0d2069c0efe5a3a1214838a
ndutiyeprosper <pr.ndutiye@gmail.com>
2023-04-26 17:10:11 -0700

    Environment

--- environ.c
+++ environ.c
@@ -1,53 +1,85 @@
 #include "shell.h"
 
-int print_env_list(info_t *info)
+/**
+ * _myenv - prints the current environment
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
+int _myenv(info_t *info)
 {
-	print_list_str(info->env_list);
+	print_list_str(info->env);
 	return (0);
 }
 
-char *get_env_var(info_t *info, const char *var_name)
+/**
+ * _getenv - gets the value of an environ variable
+ * @info: Structure containing potential arguments. Used to maintain
+ * @name: env var name
+ *
+ * Return: the value
+ */
+char *_getenv(info_t *info, const char *name)
 {
-	list_t *node = info->env_list;
-	char *var_value;
+	list_t *node = info->env;
+	char *p;
 
 	while (node)
 	{
-		var_value = starts_with(node->str, var_name);
-		if (var_value && *var_value)
-			return (var_value);
+		p = starts_with(node->str, name);
+		if (p && *p)
+			return (p);
 		node = node->next;
 	}
 	return (NULL);
 }
 
-int set_env_var(info_t *info)
+/**
+ * _mysetenv - Initialize a new environment variable,
+ *             or modify an existing one
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _mysetenv(info_t *info)
 {
-	if (info->arg_count != 3)
+	if (info->argc != 3)
 	{
-		_eputs("Incorrect number of arguments\n");
+		_eputs("Incorrect number of arguements\n");
 		return (1);
 	}
-	if (add_env_var(info, info->args[1], info->args[2]))
+	if (_setenv(info, info->argv[1], info->argv[2]))
 		return (0);
 	return (1);
 }
 
-int unset_env_var(info_t *info)
+/**
+ * _myunsetenv - Remove an environment variable
+ * @info: Structure containing potential arguments. Used to maintain
+ *        constant function prototype.
+ *  Return: Always 0
+ */
+int _myunsetenv(info_t *info)
 {
 	int i;
 
-	if (info->arg_count == 1)
+	if (info->argc == 1)
 	{
-		_eputs("Too few arguments.\n");
+		_eputs("Too few arguements.\n");
 		return (1);
 	}
-	for (i = 1; i <= info->arg_count; i++)
-		remove_env_var(info, info->args[i]);
+	for (i = 1; i <= info->argc; i++)
+		_unsetenv(info, info->argv[i]);
 
 	return (0);
 }
 
+/**
+ * populate_env_list - populates env linked list
+ * @info: Structure containing potential arguments. Used to maintain
+ *          constant function prototype.
+ * Return: Always 0
+ */
 int populate_env_list(info_t *info)
 {
 	list_t *node = NULL;
@@ -55,7 +87,6 @@ int populate_env_list(info_t *info)
 
 	for (i = 0; environ[i]; i++)
 		add_node_end(&node, environ[i], 0);
-	info->env_list = node;
+	info->env = node;
 	return (0);
 }
-
